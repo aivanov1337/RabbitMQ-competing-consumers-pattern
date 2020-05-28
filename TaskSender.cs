8f406312be76572f41e212d74fa2d75fca4a0ac4
aivanov1337 <42995537+aivanov1337@users.noreply.github.com>
2020-05-28 21:10:11 +0200

    Add files via upload

---
+++ TaskSender.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskSender
+{
+    class TaskSender
+    {
+        //initialize connection factory instance
+        ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+        public static void Main()
+        {
+            int counter = 0;
+
+            var startTimeSpan = TimeSpan.Zero;
+
+            //How often messages will be sent by the sender
+            var periodTimeSpan = TimeSpan.FromSeconds(0.25);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                SendTask("Message number " + counter++);
+            }, null, startTimeSpan, periodTimeSpan);
+
+            Console.ReadLine();
+        }
+
+        public static void SendTask(string input)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "queue2",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = input;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "queue2",
+                                     basicProperties: properties,
+                                     body: body);
+                Console.WriteLine(DateTime.Now + " - Sent: {0}", message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace Worker
+{
+    public class Worker
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "queue2",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine("Waiting for tasks.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, ea) =>
+                {
+                    var body = ea.Body.Span;
+                    var message = Encoding.UTF8.GetString(body);
+                    
+                    Console.WriteLine(DateTime.Now + " - Received: {0}", message);
+                    Console.WriteLine("Working");                   
+                    Thread.Sleep(1500); //simulates doing actual work
+                    Console.WriteLine("Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false); //acknowledges that the message was delivered
+                };
+                //consumes the message
+                channel.BasicConsume(queue: "queue2", 
+                                     autoAck: false,
+                                     consumer: consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
